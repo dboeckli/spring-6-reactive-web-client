1645c6a7c35f6ea4768faf75497a23d50d73b248
adm-boeckli <dboeckli@gmail.com>
2024-11-26 13:31:03 +0100

    improve unit tests

--- src/test/java/guru/springframework/spring6reactivewebclient/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6reactivewebclient/client/BeerClientImplTest.java
@@ -1,12 +1,14 @@
 package guru.springframework.spring6reactivewebclient.client;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import guru.springframework.spring6reactivewebclient.dto.BeerDto;
 import lombok.extern.java.Log;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -40,36 +42,54 @@ class BeerClientImplTest {
     @Test
     void listBeerMap() {
         AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        AtomicReference<List<Map>> atomicListResponse = new AtomicReference<>(new ArrayList<>());
+        
         beerClient.listBeerMap().subscribe(response -> {
             log.info("### Response: " + response);
-            atomicBoolean.set(true);
+            atomicListResponse.get().add(response);
+            if (atomicListResponse.get().size() == 3) {
+                atomicBoolean.set(true);
+            }
         });
 
         await().atMost(5, TimeUnit.SECONDS).untilTrue(atomicBoolean);
         assertTrue(atomicBoolean.get());
+        assertEquals(3, atomicListResponse.get().size());
     }
 
     @Test
     void testListBeerJsonNode() {
         AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        AtomicReference<List<JsonNode>> atomicListResponse = new AtomicReference<>(new ArrayList<>());
+        
         beerClient.listBeerJsonNode().subscribe(response -> {
             log.info("### Response: " + response);
-            atomicBoolean.set(true);
+            atomicListResponse.get().add(response);
+            if (atomicListResponse.get().size() == 3) {
+                atomicBoolean.set(true);
+            }
         });
 
         await().atMost(5, TimeUnit.SECONDS).untilTrue(atomicBoolean);
         assertTrue(atomicBoolean.get());
+        assertEquals(3, atomicListResponse.get().size());
     }
 
     @Test
     void testListBeerAsDtos() {
         AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        AtomicReference<List<BeerDto>> atomicListResponse = new AtomicReference<>(new ArrayList<>());
+        
         beerClient.listBeerAsDtos().subscribe(response -> {
             log.info("### Response: " + response);
-            atomicBoolean.set(true);
+            atomicListResponse.get().add(response);
+            if (atomicListResponse.get().size() == 3) {
+                atomicBoolean.set(true);
+            }
         });
 
         await().atMost(5, TimeUnit.SECONDS).untilTrue(atomicBoolean);
         assertTrue(atomicBoolean.get());
+        assertEquals(3, atomicListResponse.get().size());
     }
 }
