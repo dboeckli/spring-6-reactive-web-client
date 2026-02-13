b6f3204742c9020ff53316b95226f959e3d4fffb
Dominique Boeckli <dboeckli@gmail.com>
2026-02-13 07:26:13 +0100

    several fixes

--- .run/scripts/uninstall-k8s.ps1
+++ .run/scripts/uninstall-k8s.ps1
@@ -4,3 +4,7 @@ $file = Get-ChildItem -Filter spring-6-reactive-web-client-v*.tgz | Select-Objec
 $APPLICATION_NAME = Get-ChildItem -Directory | Where-Object { $_.LastWriteTime -ge $file.LastWriteTime } | Select-Object -ExpandProperty Name
 
 helm uninstall $APPLICATION_NAME --namespace spring-6-reactive-web-client
+
+kubectl delete pod -n kbe-brewery-gateway --field-selector=status.phase==Succeeded
+kubectl delete pod -n kbe-brewery-gateway --field-selector=status.phase==Failed
+
